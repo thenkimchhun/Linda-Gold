836d511888daff9df551b3907c29274c28de99c9
Test <thenkimchhun1122@gmail.com>
2024-02-08 17:01:32 +0700

    Disable date picker

---
+++ linda-gold/Utilizes/Component/CPNInputEndDateView.swift
@@ -0,0 +1,9 @@
+//
+//  CPNInputEndDateView.swift
+//  linda-gold
+//
+//  Created by Chhun on 2/8/24.
+//  Copyright © 2024 Core-MVVM. All rights reserved.
+//
+
+import Foundation
--- linda-gold/Utilizes/Component/CPNInputStartDateView.swift
+++ linda-gold/Utilizes/Component/CPNInputStartDateView.swift
@@ -7,7 +7,7 @@
 //
 import UIKit
 
-class CPNTextFieldDateView: BaseView {
+class CPNInputStartDateView: BaseView {
     let containerView = UIView()
     
     override func setupComponent() {
